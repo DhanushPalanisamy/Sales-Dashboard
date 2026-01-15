484a582f74947e99f2cca75408f0584142e7f68f
DhanushPalanisamy <dhanush8807474644@gmail.com>
2026-01-15 10:31:38 +0530

    Fix obsolete app.run_server method to app.run

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)), debug=False)
 
